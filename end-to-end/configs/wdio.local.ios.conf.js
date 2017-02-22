cdf2db5461b7002354e37e073739dc759bfbfa42
Omer Baki <bakiomer@>
2017-02-22 16:05:38 +0200

    Run test on iOS

---
+++ end-to-end/configs/wdio.local.ios.conf.js
@@ -0,0 +1,115 @@
+module.exports.config = {
+
+    // ==================
+    // Specify Test Files
+    // ==================
+    specs: ['tests/test.spec.js'],
+
+    // ============
+    // Capabilities
+    // ============
+
+    capabilities: [{
+        automationName: "XCUITest",
+        browserName: '',
+        deviceName: 'iOS',
+        bundleId: 'org.reactjs.native.example.DemoApp',
+        udid: '5B9E8C77-C77A-46E1-9F84-4273E18E496F',
+        platformName: 'iOS',
+        //platformVersion: options.platformVersion,
+        newCommandTimeout :300,
+        autoAcceptAlerts: true,
+        autoLaunch: false,
+        iosInstallPause: 8000,
+        //xcodeConfigFile: path.resolve(`./configs/signing.xcconfig`).replace(/\\/g,'/'),
+        noReset : true
+    }],
+
+    port: 4723,
+
+    maxInstances: 1,
+    //
+
+    // ===================
+    // Test Configurations
+    // ===================
+    // By default WebdriverIO commands are executed in a synchronous way using
+    // the wdio-sync package. If you still want to run your tests in an async way
+    sync: true,
+    //
+    // Level of logging verbosity: silent | verbose | command | data | result | error
+    logLevel: 'silent',
+    //
+    // Enables colors for log output.
+    coloredLogs: true,
+    //
+    // Set a base URL in order to shorten url command calls. If your url parameter starts
+    // with "/", then the base url gets prepended.
+    baseUrl: 'http://localhost',
+    //
+    // Default timeout for all waitFor* commands.
+    waitforTimeout: 30000,
+    //
+    // Default timeout in milliseconds for request if Selenium Grid doesn't send response
+    connectionRetryTimeout: 900000,
+    //
+    // Default request retries count
+    connectionRetryCount: 0,
+    // Make sure you have the wdio adapter package for the specific framework installed before running any tests.
+    framework: 'mocha',
+    //
+    // Test reporter for stdout.
+    // see also: http://webdriver.io/guide/testrunner/reporters.html
+    reporters: ['dot', 'junit'],
+    //reporterOptions: {
+    //    junit: {
+    //        outputDir: './reports',
+    //        outputFileFormat: (x)=> {
+    //            const apkName = x.capabilities.split('.').pop().split('\\').pop();
+    //            return `${apkName}.xml`;
+    //        }
+    //    }
+    //},
+    //
+    // Options to be passed to Mocha - See the full list at http://mochajs.org/
+    mochaOpts: {
+        ui: 'bdd',
+        compilers: ['js:babel-core/register'],
+        timeout: 3000000
+    },
+    //
+    // =====
+    // Hooks
+    // =====
+    before: function (capabilities, specs) {
+        try{
+            const chai = require('chai');
+            global.assert = chai.assert;
+            global.expect = chai.expect;
+            chai.Should();
+        }
+        catch (err){
+            console.log('Failed to setup test', err);
+            throw err;
+        }
+    },
+
+    afterCommand: function (commandName, args, result, error) {
+    },
+
+    onError: function(e) {
+        console.log(`error occured - ${e.message}`);
+    }
+
+    //Other possible hooks:
+    //
+    // beforeSuite: function (suite) {},
+    // afterSuite: function (suite) {},
+    // beforeHook: function () {},
+    // afterHook: function () {},
+    // onPrepare: function (config, capabilities) {},
+    // beforeCommand: function (commandName, args){},
+    // afterTest: function (test) {},
+    // afterSuite: function (suite) {},
+    // onComplete: function(exitCode) {}
+};
